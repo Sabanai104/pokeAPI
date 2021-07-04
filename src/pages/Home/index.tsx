86171d841830164399105d1d14b33324e1d81778
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2021-07-04 14:41:03 -0300

    structure the project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { HelloWorld } from './style';
+
+const Home = () => {
+    return(
+        <View>
+            <HelloWorld>
+                Hello World!
+            </HelloWorld>
+        </View>
+    )
+}
+
+export default Home;
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const HelloWorld = styled.Text `
+    background-color: blue;
+    width: 100%;
+    height: 20%;
+    font-size: 20px; 
+`
